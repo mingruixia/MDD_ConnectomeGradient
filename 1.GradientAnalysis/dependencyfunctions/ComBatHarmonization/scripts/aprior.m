a988caa166e4cdeed7bf83a855ba655ea1f59457
mingruixia <mingruixia@gmail.com>
2021-08-30 20:03:51 +0800

    Delete 1.GradientAnalysis directory

--- 1.GradientAnalysis/dependencyfunctions/ComBatHarmonization/scripts/aprior.m
+++
@@ -1,5 +0,0 @@
-function y = aprior(gamma_hat)
-	m = mean(gamma_hat);
-  	s2 = var(gamma_hat);
-  	y=(2*s2+m^2)/s2;
-end
--- 1.GradientAnalysis/dependencyfunctions/ComBatHarmonization/scripts/postvar.m
+++
@@ -1,3 +0,0 @@
-function y = postvar(sum2,n,a,b)
-	y=(.5.*sum2+b)./(n/2+a-1);
-end
